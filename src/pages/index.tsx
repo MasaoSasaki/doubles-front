15a8d5fbb2a7d7701d70755897aa9d6bc3d7e0ea
MasaoSasaki <daikichi852@icloud.com>
2021-12-01 08:05:07 +0900

    Add test api request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Layout } from "src/components/layout";
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/repos/zeit/next.js";
 
 const Home: NextPage = () => {
   const handleClick = () => {
-    window.alert("Hello, World!");
+    fetch(url)
+      .then((r) => {
+        r.json();
+        console.log(r, "Success");
+      })
+      .then((j) => console.log(j, "fail"));
   };
 
   return (
@@ -14,7 +22,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2>Home</h2>
-      <button onClick={handleClick}>Button</button>
+      <p>人数：</p>
+      <input type="number" value={4} name="" id="" />
+      <button onClick={handleClick}>生成</button>
     </Layout>
   );
 };
