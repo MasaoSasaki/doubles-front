3fa76af46ff667e80e92d3ed16b8212a08691888
MasaoSasaki <daikichi852@icloud.com>
2021-12-13 08:13:50 +0900

    :tada: Creating algorithm

--- src/algorithms/doubles.ts
+++ src/algorithms/doubles.ts
@@ -1,3 +1,16 @@
-export const Test = () => {return alert("test")};
+export const MatchList = (entry: number) => {
+  let match: number[] = [];
 
-Test();
+  // 一試合分の配列を作る
+  while (match.length < entry) {
+    let playerNumber = Math.floor(Math.random() * (entry + 1 - 1)) + 1;
+
+    // 重複を排除
+    if (match.includes(playerNumber)) continue;
+    match = [...match, playerNumber];
+    playerNumber = 0;
+  }
+  return match;
+};
+
+MatchList(5);
